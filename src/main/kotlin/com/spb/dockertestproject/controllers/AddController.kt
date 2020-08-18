fdfb2b19806365d861a38b813d50d9868fe4aad1
Ainur-sr <ainshigapov@gmail.com>
2020-08-19 00:23:18 +0300

    added view

--- src/main/kotlin/com/spb/dockertestproject/controllers/AddController.kt
+++ src/main/kotlin/com/spb/dockertestproject/controllers/AddController.kt
@@ -2,24 +2,40 @@ package com.spb.dockertestproject.controllers
 
 import com.spb.dockertestproject.service.KeyMapperService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.*
 
 
 @Controller
-@RequestMapping("add")
 class AddController {
+
+    @Value("\${smlr.prefix}")
+    private lateinit var prefix: String
 
     @Autowired lateinit var service: KeyMapperService
 
-    @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @PostMapping(path = ["add"],
+            consumes = [MediaType.APPLICATION_JSON_VALUE],
+            produces = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseBody
-    fun add(@RequestBody request: AddRequest) =
+    fun addRest(@RequestBody request: AddRequest) =
             ResponseEntity.ok(AddResponse(request.link, service.add(request.link)))
+
+    @PostMapping(path = ["addhtml"])
+    fun addHtml(model: Model, @RequestParam(value = "link", required = true) link: String):String {
+        val result = add(link)
+        model.addAttribute("link", result.link)
+        model.addAttribute("keyed", prefix + result.key)
+        return "result"
+    }
 
     data class AddRequest(val link: String)
     data class AddResponse(val link: String, val key: String)
+
+    private fun add(link: String) = AddResponse(link, service.add(link))
 
 }
--- src/test/kotlin/com/spb/dockertestproject/controllers/RedirectControllerTest.kt
+++ src/test/kotlin/com/spb/dockertestproject/controllers/RedirectControllerTest.kt
@@ -16,6 +16,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner
 import org.springframework.test.context.web.WebAppConfiguration
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.*
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 import org.springframework.web.context.WebApplicationContext
@@ -66,9 +67,16 @@ class RedirectControllerTest {
     private val BAD_PATH = "badrequest"
     private val NOT_FOUND: Int = 404
 
-    @Test fun controllerMustReturn404IfBadKey() {
+    @Test
+    fun controllerMustReturn404IfBadKey() {
         mockMvc.perform(get("/$BAD_PATH"))
                 .andExpect(status().`is`(NOT_FOUND))
+    }
+
+    @Test
+    fun homeWorksFine() {
+        mockMvc.perform(get("/"))
+                .andExpect(MockMvcResultMatchers.view().name("home"))
     }
 
 }
