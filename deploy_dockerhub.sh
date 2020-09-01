64664bc650188722ea3d493a7c032c65d87bedf3
Ainur-sr <ainshigapov@gmail.com>
2020-09-01 17:27:56 +0300

    added heroku deploy file

--- deploy_dockerhub.sh
+++ deploy_dockerhub.sh
@@ -1,6 +1,6 @@
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
-heroku container:login
 heroku plugins:install heroku-container-registry
+heroku container:login
 docker login -e _ -u _ --password=$HEROKU_API_KEY registry.heroku.com
 heroku container:push web --app $HEROKU_APP_NAME
 heroku container:release web --app=$HEROKU_APP_NAME
